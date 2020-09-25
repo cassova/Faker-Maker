60b36755679cad0aec2b454d4c367cb899a93abb
cassova <miller.nicholas.a@gmail.com>
2020-09-25 14:31:04 +0900

    initial

--- README.md
+++ README.md
@@ -1,2 +1,112 @@
 # Faker-Maker
 This module creates fake pandas dataframes using a custom domain specific language
+
+## The `FakeDataFrameBuilder` class
+The heavy lifter for this assignment is named `FakeDataFrameBuilder`.  It resides in the file `faker_maker.py`.  It is designed to be used as the magic function `%%fakedata` which is demonstrated in this notebook.  Using it as a class instead of a magic function is also possible.  Create an instance of it as such:
+```
+from faker_maker import FakeDataFrameBuilder
+cls = FakeDataFrameBuilder(text)
+```
+Where `text` is the multi-line text blob containing one or more dataframe definitions.  The worker function that parses the text blob and builds the dataframes is `parse` and is called like this:
+```
+cls.parse()
+```
+Access the results within the variable `dataframes`.  Since multiple dataframes might be built, this object is a dictionary of each dataframe indexed by the name.
+```
+persons = cls.dataframes['persons']
+```
+Below are a few notes on the structure and operation that may differ from the request and provide more clarity.
+
+## An attempt at a more formal grammar
+
+```
+function_to_call  ::= <wordcharacters>
+parameters        ::= "" | "(" ( wordcharacters | number ) ")"
+as_name           ::= "" | "as" <whitespace> <wordcharacters>
+column_name       ::= as_name | function_to_call
+reference         ::= "" |  "[" number1, number2 "]"
+unique_mark       ::= "" | "*"
+column_definition ::= <function_to_call> <parameters> <whitespace> \
+                      <as_name> <whitespace> <reference> <unique_mark>
+df_sep            ::= "--" ("-"*)
+df_definition     ::= <wordcharacters> <newline> <df_sep> <newline> \
+                      (<column_definition>*) <newline> <newline>
+language_spec     ::= <def_definition>*
+```
+Source: Prof. Christopher Brooks (brooksch@umich.edu)
+
+### Table Definitions
+The table defintion has been expanded from the request to include a row count.  The default remains `99` records if no row is provided but now a new value can be specified using the special syntax ` {#}` after the table name.  For example: 
+
+```
+purchases {30}
+--------------
+```
+
+If tables are to have parent-child relationships and linked by a reference (see below), then the parent defintion should be provided first and the children after.
+
+### Comments
+The domain specific language invented for this problem was expanded to support comments similar to python using a `#`.  Comments are not supported throughout.  Examples where comments can be added:
+ - After a table name (and optional row count)
+ - After a field defintion
+ - Before a field defintion (to comment out the field)
+
+
+### Unique Values
+The "unique" constraint is a bit ambiguous because let's say you have 2 columns (fields) that are defined as being unique within a dataset like in the example that was provided.  This could be interpreted in two ways: 
+
+ 1. The combination of the 2 values must be unique within the dataset
+ 2. Every value in the column must be unique
+ 
+For this implementation, I found that the latter is more flexible in that it satisifies both of these points.  However, this comes with caveats that should be noted. The most obvious is a limitation of values.  For example, if you specify a column to contain a random number of length 2 and also unique, it is impossible to have a dataset with over 100 unique entries.  In fact, in how this is implemented, it is likely it would throw an exception much earlier due to how we limit the number times we look for a unique value.  Since we're using Faker to generate unique values, it should be kept in mind that there will be some limit to the number of unique values for each function.  For example, `last_name` might only generate 100 unique last names.  In my tests, I was getting duplicate values quite early on so its possible a unique set of last_names may randomly throw an error for a set of 99 unique last names.  It is recommeneded to read the Faker documentation for more information on what the limitations of each function are if you start getting errors about 'Maxed out unique checks'.
+
+ - Future enhancement: Create a solution to #1 above.
+ 
+### Parameters
+All parameters must define the varable they are referencing in the parameter list.  This is necessary for how the parameter dictionary structure is built.  It's also good practice because it makes the parameter purpose much clearer.  For example:
+```
+random_number(digits=5) as customer_number
+```
+
+### References
+This was expanded to allow for a comma delimited list of references and is demonstrated below.  The purpose is in the case that you want to drill down into a narrower and narrower set of values.  In the example below, not all persons will have purchases and not all purchases will have comments.  But, all comments should have one or more purchases and all purchases should have one person (since customer_number is unique on persons).  Here's how this would look:
+
+```
+persons
+-------
+random_number(digits=5) as customer_number [1]*
+<other fields...>
+
+purchases
+-------
+random_number(digits=5) as customer_number [1,2]
+<other fields...>
+
+comments
+-------
+random_number(digits=5) as customer_number [2]
+<other fields...>
+```
+
+NOTE: The `function_name` is not actually used on child tables since the values are actually retrieved from the parent table.  In the above example, `purchases` and `comments` all get their values from `persons` so the `random_number(digits=5)` is actually not used or validated against.
+ 
+### Repeat Data (Goal = 25% Duplicates)
+This touches on the same vain as unique since we're doing some manual tricks to try to meet this 25% goal.  The solution I chose for this is to generate 75% of the values (note: this is controlled by the field `FakeDataFrameBuilder.repeat_threshold`) from Faker.  It then analyzes the uniqueness of the results.  If 25% or more duplicates already result, we just hit up the Faker for the remaining 25% (i.e. tough luck, go look at the other guy).  If it's >0% but <25%, then we salt it by hitting up the Faker again.
+
+For example, if the uniqueness value comes back as 20% duplicates in the first 75% generated, it's pretty safe to say that the Faker is already doing a good job at getting close to our 25% limit.  So we'll get 20% more values from the faker.
+
+For the remaining percent (or 25% if there were no unique values), we randomly sample from within our generated list.  This will obviously result in duplicates or possibly even triplicates or more.
+
+
+### Seeding
+I wanted a method to regenerate the same dataset so I implemented a `seed()` function.  This generally works but doesn't seem to work well with my random_number() generator for customer_number yet.  Still need to figure that one out...
+
+ - Issue - `seed()` still generates random customer_numbers
+
+The seed can now be set on the magic function call line using the syntax `seed=0`.
+
+### Language
+The language can also be defined on the magic function line using the syntax `lang=jp_JP`.
+ 
+### Conclusion
+This was created by Nicholas Miller (nmill@umich.edu) from designs provided above by Prof. Christopher Brooks (brooksch@umich.edu).  Submitted 2020-09-23.
---
+++ faker_maker.py
@@ -0,0 +1,289 @@
+# Author: Nicholas Miller (nmill@umich.edu)
+# Created: 2020-09-23
+
+from IPython.core.magic import (Magics, magics_class, register_cell_magic,
+                                line_cell_magic, needs_local_scope)
+import numpy as np
+import pandas as pd
+from faker import Faker
+import random
+import re
+
+class FakeDataFrameParserException(Exception):
+    pass
+
+class FakeDataFrameAssembleException(Exception):
+    pass
+
+class FakeDataFrameBuilder():
+    __faker = Faker()
+    __lines = []
+    __reference_dict = {}
+    __default_dataframe_size = 99
+    __seed = np.NaN
+    __field_matcher = """
+        ^(?P<function_to_call>[\w]+)
+        (\s*\((?P<parameters>[\w=\-\'\.]+(,[\w=\-\'\.]+)*)\))*
+        (\s+as\s+(?P<field_name>\w+))*
+        (\s+\[(?P<reference>[\d,]+)\])*
+        (\s*(?P<unique_mark>\*))*
+        (\s+\#(?P<comment>.*))*$
+    """
+    
+    repeat_threshold = 0.25    # Try to repeat values 20% of the time
+    dataframes = {}
+    
+    def __init__(self):
+        pass
+        
+    def __init__(self, text_blob, lang="en_US"):
+        self.__faker = Faker(lang)
+        self.__lines = text_blob.split('\n')
+
+    def seed(self, s):
+        self.__seed = s
+        self.__faker.seed_instance(s)
+        random.seed(s)
+        np.random.seed(s)
+        
+    
+    def __get_faker_data(self, function_name, params):
+        func_to_run = getattr(self.__faker, function_name)
+        return func_to_run(**params)
+    
+    def __assemble_reference_column(self, references, field, details, unique_fields, size):
+        func = details['function_to_call']
+        args = details['parameters']
+        result = []
+        
+        # First identify which table it is we're working on
+        for r in references.split(','):
+            ref_list = self.__reference_dict[r]
+            for d in ref_list:
+                # Check if an already built table contains our reference (is in ref_list)
+                try:
+                    # Don't like how this is seeded:
+                    #return self.dataframes[d[0]][d[1]].sample(n=size, random_state=self.__seed).to_list()
+                    return random.sample(self.dataframes[d[0]][d[1]].to_list(),size)
+                except:
+                    pass
+            # if we got here, we couldn't find a df with any reference values so just generate new values
+            if field in unique_fields:
+                return self.__assemble_unique_column(field, details, size)
+            else:
+                return self.__assemble_standard_column(field, details, size)
+    
+    def __assemble_unique_column(self, field, details, size):
+        func = details['function_to_call']
+        args = details['parameters']
+        result = []
+        
+        for i in np.arange(size):
+            # If we have a unique violation, we will attempt again up to a max of this # of times
+            max_unique_checks = 10
+            unique_count = 0
+            while True:
+                # Loop until we find a unique value or run out of tries
+                value = self.__get_faker_data(func, args)
+                if unique_count >= max_unique_checks:
+                    raise FakeDataFrameAssembleException('Maxed out unique checks for field {field}')
+                elif value in result:
+                    #print(f'Found Duplicate for field {field} with value {value}')
+                    unique_count += 1
+                else:
+                    break
+            result.append(value)
+        return result
+    
+    def __assemble_standard_column(self, field, details, size):
+        func = details['function_to_call']
+        args = details['parameters']
+        result = []
+        
+        # (A-Group) Start building a list of random values
+        asize = np.floor(size * (1-self.repeat_threshold))
+        for i in np.arange(asize):
+            result.append(self.__get_faker_data(func, args))
+            
+        # Let's see how unique our A-group is before we start our overlap pass
+        # If not all values our unique, let's salt it so we hopefully endup closer to our
+        # random threshold value
+        asize_salt = 0
+        unq = []
+        unq_perc = 1 - len([unq.append(x) for x in result if x not in unq]) / len(result)
+        if unq_perc > 0:
+            if unq_perc > self.repeat_threshold:
+                asize_salt = size - asize
+            else:
+                asize_salt = np.floor(unq_perc * asize)
+            #print(f'Field {field} is {unq_perc} unique and will get {asize_salt} more values')
+            for i in np.arange(asize_salt):
+                result.append(self.__get_faker_data(func, args))
+        
+        # (B-Group) Sample from our existing list to ensure we get an overlap of data (e.g. duplicates)
+        bsize = size - asize - asize_salt
+        for i in np.arange(bsize):
+            result.append(random.sample(result,1)[0])
+            
+        random.shuffle(result)
+        return result
+        
+    def __assemble_dataframe(self, name, size, fields):
+        # Identify which fields are unique
+        unique_fields = [k for k,v in fields.items() if v['unique_mark'] == '*']
+        
+        # Here we will create our dictionary that contains our fake data to be put into DataFrames
+        dataframe_dict = {}
+        for field, details in fields.items(): # loop over each field
+            if details['reference'] is not None:
+                dataframe_dict[field] = self.__assemble_reference_column(details['reference'], field, details,
+                                                                         unique_fields, size)
+            elif field in unique_fields:
+                dataframe_dict[field] = self.__assemble_unique_column(field, details, size)
+            else:
+                dataframe_dict[field] = self.__assemble_standard_column(field, details, size)
+        self.dataframes[name] = pd.DataFrame(dataframe_dict)
+    
+    def __parse_parameters(self, params):
+        # This will build a parameter dictionary
+        # Example: my_func('first',2=2,third=3,4) --> {0: "'first'", '2': 2.0, 'third': 3.0, 3: 4.0}
+        if params is None:
+            return {}
+        else:
+            param_split = params.split(',')
+            param_dict = {}
+            for i, param in enumerate(param_split):
+                parts = re.match(r'^((?P<key>[\w\-]+)\=)*(?P<value>[\w\-\.]+|\'[\w\-\.]+\')$', param)
+                if parts is None:
+                    raise FakeDataFrameParserException(f'Unparsable parameter: {param}')
+                key = parts.groupdict()['key']
+                if key is None:
+                    key = i
+                # check if the value contains only numbers, if so, convert to number
+                if re.match(r'^[\d\.]+$', parts.groupdict()['value']):
+                    param_dict[key] = float(parts.groupdict()['value'])
+                else:
+                    param_dict[key] = parts.groupdict()['value']
+        #print (f'Parameters = {param_dict}')
+        return param_dict
+    
+    def __parse_reference(self, reference, table, field):
+        for r in reference.split(','):
+            try:
+                ref_list = self.__reference_dict[r] 
+                ref_list.append((table, field))
+            except:
+                self.__reference_dict[r] = [(table, field)]
+    
+    def parse(self):
+        if len(self.__lines) < 2:
+            raise FakeDataFrameParserException('Not enough lines to process')
+        
+        new_dataframe_name = ""
+        new_dataframe_size = 0
+        new_dataframe_fields = {}
+        next_is_header = True
+        next_is_seperator = False
+        
+        for line in self.__lines:
+            if next_is_header:
+                # Handle extra whitespace or comments between dataframe definitions
+                parts = re.match(r'^\s*(\#.*)*$', line)
+                if parts is None:
+                    # Here we're expecting a header (i.e. a dataframe name)
+                    parts = re.match(r'^(?P<name>[\w]{1}[\w\-]+)(\s*{(?P<size>\d+)\})*(?P<comment>(\s+[#]{1}.*|\s*)$)', line)
+                    if parts is None:
+                        raise FakeDataFrameParserException(f'Unparsable header line: {line}')
+                    new_dataframe_name = parts.groupdict()['name']
+                    if parts.groupdict()['size'] is None:
+                        new_dataframe_size = self.__default_dataframe_size
+                    else:
+                        new_dataframe_size = int(parts.groupdict()['size'])
+                    next_is_header = False
+                    next_is_seperator = True
+            elif next_is_seperator:
+                parts = re.match(r'^(?P<sep>[\-]{2,})(?P<comment>(\s+[#]{1}.*|\s*)$)', line)
+                if parts is None:
+                    raise FakeDataFrameParserException(f'Unparsable seperator line: {line}')
+                next_is_seperator = False
+            else: # We either have a field or empty line
+                parts = re.match(r'^\s*$', line)
+                if parts is not None:
+                    # We have an empty line so we'll assume this dataframe definition is done - could be smarter
+                    if len(new_dataframe_fields) < 1:
+                        raise FakeDataFrameParserException(f'Unexpected empty dataframe: {new_dataframe_name}')
+                    self.__assemble_dataframe(new_dataframe_name, new_dataframe_size, new_dataframe_fields)
+                    
+                    # Clear our values
+                    new_dataframe_name = ""
+                    new_dataframe_fields = {}
+                    next_is_header = True
+                else:
+                    # This a field line
+                    if re.match(r'^#', line):
+                        pass
+                    else:
+                        # Here we're expecting a field definition
+                        parts = [item.groupdict() for item in re.finditer(self.__field_matcher,line,re.VERBOSE)]
+                        if len(parts) < 1:
+                            raise FakeDataFrameParserException(f'Unparsable field line: {line}')
+                        else:
+                            if parts[0]['field_name'] is None:
+                                # Here we'll assume the field name is the function_to_call
+                                i = 1
+                                field_name = parts[0]['function_to_call']
+                                while True:
+                                    # If we have the same function defined multiple times, we'll give a unique name
+                                    if field_name in new_dataframe_fields.keys():
+                                        field_name = parts[0]['function_to_call'] + str(i)
+                                        i += 1
+                                    else:
+                                        break
+                            else:
+                                field_name = parts[0]['field_name']
+                            del parts[0]['field_name']
+                            parts[0]['parameters'] = self.__parse_parameters(parts[0]['parameters'])
+                            if parts[0]['reference'] is not None:
+                                self.__parse_reference(parts[0]['reference'], new_dataframe_name, field_name)
+                            new_dataframe_fields[field_name] = parts[0]
+                        
+        # All done with our for-loop, let's call assemble if we have an unassembled dataframe
+        if len(new_dataframe_name) > 0:
+            if len(new_dataframe_fields) < 1:
+                raise FakeDataFrameParserException(f'Unexpected empty dataframe: {new_dataframe_name}')
+            self.__assemble_dataframe(new_dataframe_name, new_dataframe_size, new_dataframe_fields)
+        
+        if len(self.dataframes) == 0:
+            raise FakeDataFrameParserException(f'No dataframes were created')
+        #print (f'References = {self.__reference_dict}')
+    
+
+    
+@magics_class
+class AutoMagics(Magics):
+    @needs_local_scope
+    @line_cell_magic
+    def fakedata(self, line, cell, local_ns=None):
+        params = {}
+        if len(line) > 0:
+            params = self.__get_magic_params(line)
+        if 'lang' in params:
+            cls = FakeDataFrameBuilder(cell, params['lang'])
+        else:
+            cls = FakeDataFrameBuilder(cell)
+        if 'seed' in params:
+            cls.seed(int(params['seed']))
+        cls.parse()
+        for name, df in cls.dataframes.items():
+            local_ns[name] = df
+    
+    def __get_magic_params(self, line):
+        result = {}
+        r = re.findall("([\w]+)=([\w]+)", line)
+        for k,v in r:
+            result[k] = v
+        return result
+
+
+def load_ipython_extension(ipython):
+    ipython.register_magics(AutoMagics)
